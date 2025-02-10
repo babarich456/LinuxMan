5f73da38ba058e53908ce64c33afa44c95b9aad5
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-10 19:57:49 +0300

    Update menu.sh
    Signed-off-by: Muhammet Atilla <muhammetcanaltan14@gmail.com>

--- menu.sh
+++ menu.sh
@@ -3,10 +3,19 @@ orange='\e[38;5;166m'
 clear
 sleep 1.5
 echo -e "$default"
-figlet '\e[38;5;166m' " TermuxButLinux "
+banner() {
+
+printf "\e[1;92m _____                              ____        _   _     _\e[0m\n"
+printf "\e[1;92m|_   _|__ _ __ _ __ ___  _   ___  _| __ ) _   _| |_| |   (_)_ __  _   ___  __\e[0m\n"
+printf "\e[1;92m| |/ _ \ '__| '_ ` _ \| | | \ \/ /  _ \| | | | __| |   | | '_ \| | | \ \/ /\e[0m\n"
+printf "\e[1;92m| |  __/ |  | | | | | | |_| |>  <| |_) | |_| | |_| |___| | | | | |_| |>  <\e[0m\n"
+printf "\e[1;77m  |_|\___|_|  |_| |_| |_|\__,_/_/\_\____/ \__,_|\__|_____|_|_| |_|\__,_/_/\_\\e[0m\n"
+printf "\n"
+printf "\e[1;77m\e[45m  Github: https://github.com/babarich456/TermuxButLinux   \e[0m\n"
+printf "\n"
+}
 echo "| Note: If bash metasploitframework.sh doesn't work, try msfconsole.sh |"
 echo "| What would you like to do?                                           |"
-echo "| Github: https://github.com/babarich456/TermuxButLinux.git            |"
 echo "| bash kalilinux.sh                                                    |"
 echo "| bash hydra.sh                                                        |"
 echo "| bash aircrack-ng.sh                                                  |"
