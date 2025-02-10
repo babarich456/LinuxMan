ef08853d503c92b800b3d6d036de9c93ec083aad
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-10 08:40:12 -0500

    Update menu.sh
    Signed-off-by: Muhammet Atilla <muhammetcanaltan14@gmail.com>

--- menu.sh
+++ menu.sh
@@ -1,24 +1,12 @@
-g="\033[1;32m"
-r="\033[1;31m"
-b="\033[1;34m"
-w="\033[0m"
-red='\e[1;31m'
-default='\e[0m'
-yellow='\e[0;33m'
-orange='\e[38;5;166m'
-green='\033[92m'
-
 clear
 sleep 1.5
 echo -e "$default"
-figlet " Linux Man "
-echo "Available Tools: "
-echo "bash nmap.sh"
-echo "bash metasploit.sh"
-echo "bash ddos.sh"
-echo "bash dos.sh"
-echo "bash rat.sh"
-echo "Example: "
-echo "bash nmap.sh"
-echo "Just Linux"
-echo "Write One: "
+figlet " TermuxButLinux "
+echo "| What would you like to do? /"
+echo "| bash kalilinux.sh   |"
+echo "| bash hydra.sh       |"
+echo "| bash nmap.sh        |"
+echo "| bash metasploit.sh  |"
+echo "| Write One:          |"
+echo "| Example:            |"
+echo "| bash nmap.sh        |"
