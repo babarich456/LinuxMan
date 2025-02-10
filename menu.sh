c31d4012d0492744af7c4ed3e4c7b333dcee35bc
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-10 07:55:57 -0500

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,28 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+figlet " Linux Man "
+echo "Available Tools: "
+echo "bash nmap.sh"
+echo "bash metasploit.sh"
+echo "bash rat.sh"
+echo "bash phisher.sh"
+echo "bash ghost.sh"
+echo "bash ip.sh"
+echo "bash dos.sh"
+echo "bash ddos.sh"
+echo "bash userfinder.sh"
+
+echo "Example: "
+echo "bash nmap.sh"
+ecgo "Just Linux"
