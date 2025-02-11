b10c8ccd32c437cc91766f063ca7485e049e3ef3
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-11 07:38:44 -0500

    Update menu.sh
    Signed-off-by: Muhammet Atilla <muhammetcanaltan14@gmail.com>

--- menu.sh
+++ menu.sh
@@ -4,10 +4,11 @@ clear
 sleep 1.5
 echo -e "$default"
 figlet " TermuxButLinux "
-echo "| Note: If bash metasploitframework.sh doesn't work, try msfconsole.sh  |"
+echo "| Note: If bash msfconsole.sh doesn't work, try metasploitframework.sh  |"
 echo "| GitHub: https://github.com/babarich456/TermuxButLinux.git             |" 
 echo "| What would you like to do?                                            |"
 echo "| bash kalilinux.sh                                                     |"
+echo "| bash root.sh                                                          |"
 echo "| bash hydra.sh                                                         |"
 echo "| bash aircrack-ng.sh                                                   |"
 echo "| bash nmap.sh                                                          |"
