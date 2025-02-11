2d4daa3b90580f0d21266219e2da91b11415caa6
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-11 07:54:49 -0500

    Update menu.sh
    Signed-off-by: Muhammet Atilla <muhammetcanaltan14@gmail.com>

--- menu.sh
+++ menu.sh
@@ -1,20 +1,29 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
 orange='\e[38;5;166m'
+green='\033[92m'
+
 
 clear
 sleep 1.5
 echo -e "$default"
-figlet " TermuxButLinux "
-echo "| Note: If bash msfconsole.sh doesn't work, try metasploitframework.sh  |"
-echo "| GitHub: https://github.com/babarich456/TermuxButLinux.git             |" 
-echo "| What would you like to do?                                            |"
-echo "| bash kalilinux.sh                                                     |"
-echo "| bash sqlmap.sh                                                        |"
-echo "| bash root.sh                                                          |"
-echo "| bash hydra.sh                                                         |"
-echo "| bash aircrack-ng.sh                                                   |"
-echo "| bash nmap.sh                                                          |"
-echo "| bash metasploitframework.sh                                           |"
-echo "| bash msfconsole.sh                                                    |"
-echo "| Write One:                                                            |"
-echo "| Example:                                                              |"
-echo "| bash nmap.sh                                                          |"
+yellow=figlet " TermuxButLinux "
+default=echo "| Note: If bash msfconsole.sh doesn't work, try metasploitframework.sh  |"
+red=echo "| GitHub: https://github.com/babarich456/TermuxButLinux.git                 |" 
+green=echo "| What would you like to do?                                              |"
+g="| bash kalilinux.sh                                                                |"
+r="| bash sqlmap.sh                                                                   |"
+b="| bash root.sh                                                                     |"
+w="| bash hydra.sh                                                                    |"
+g="| bash aircrack-ng.sh                                                              |"
+r="| bash nmap.sh                                                                     |"
+b="| bash metasploitframework.sh                                                      |"
+w="| bash msfconsole.sh                                                               |"
+g="| Write One:                                                                       |"                                                        |"
+r="| Example:                                                                         |"
+b="| bash nmap.sh                                                                     |"
